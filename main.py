8843e8cf3c8c71215a9171721624be599dfb8397
fibetus <s30331@pjwstk.edu.pl>
2025-03-18 17:06:09 +0100

    Add --help and -h flag to the script

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple Snake Game in Python")
+
+
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
+

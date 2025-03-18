b9579b32811a0b8206c806adb6ac3fa3c21d4ea0
fibetus <s30331@pjwstk.edu.pl>
2025-03-18 17:16:57 +0100

    Add --history argument

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Simple Snake Game in Python")
+    parser.add_argument("--history", help="Displays the history of Snake Game", action="store_true")
 
 
     args = parser.parse_args()
+    if args.history:
+        print("History of the Snake Game:\n"
+              "The Snake genre began with the 1976 arcade video game Blockade developed and published by Gremlin Industries.\n"
+              "The first home computer version 'Worm' was programmed by Peter Trefonas for the TRS-80 and published in 1978.\n"
+              "Fun Fact: The Snake on IBM PC was rendered in a text mode!")
 
 if __name__ == '__main__':
     main()

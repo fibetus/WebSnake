05739054550cc291ebff113bc3479bed200261c1
fibetus <s30331@pjwstk.edu.pl>
2025-04-07 22:55:27 +0200

    Add UI grid with bottom panel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+from engine import move_snake, check_collision, is_within_bounds, increase_speed, Game
+from ui import UI
 
 
 
@@ -14,6 +16,11 @@ def main():
               "The Snake genre began with the 1976 arcade video game Blockade developed and published by Gremlin Industries.\n"
               "The first home computer version 'Worm' was programmed by Peter Trefonas for the TRS-80 and published in 1978.\n"
               "Fun Fact: The Snake on IBM PC was rendered in a text mode!")
+
+    game = Game(board_size=(10, 10), initial_position=(5, 5), direction="right")
+
+    ui = UI(game)
+    ui.start()
 
 if __name__ == '__main__':
     main()

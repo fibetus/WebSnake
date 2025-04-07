6a3f3a672c291e27a7eea87bf6f9fc21bf7ad48b
fibetus <s30331@pjwstk.edu.pl>
2025-04-07 23:57:27 +0200

    Add checking collision with snake own body

--- engine/engine_core.py
+++ engine/engine_core.py
@@ -14,10 +14,18 @@ class Game:
 
         new_head = move_snake(self.snake[0], self.direction)
 
+        # Checking if snake isn't withing bounds
         if not is_within_bounds(new_head, self.board_size) or new_head == self.snake:
             self.game_over = True
             return
 
+        # Checking if snake isn't inside itself
+        for segment in self.snake[:-1]:
+            if check_collision(new_head, segment):
+                self.game_over = True
+                return
+
+        # Add "tail" to snake (more like adding new head and old head becomes tail)
         self.snake.insert(0, new_head)
 
         if check_collision(new_head, self.food):
@@ -25,7 +33,7 @@ class Game:
             self.speed = increase_speed(self.score)
             self.spawn_food()
         else:
-            self.snake.pop()
+            self.snake.pop() # Pop if we don't eat anything
 
     def spawn_food(self):
         from random import randint
@@ -37,14 +45,14 @@ class Game:
                 break
 
 
-    # checking if we don't change direction into snake body
+    # Checking if we don't change direction into snake body
     def change_direction(self, new_direction):
         opposites = {"up": "down", "down": "up", "left": "right", "right": "left"}
         if new_direction != opposites.get(self.direction):
             self.direction = new_direction
 
 
-
+# Snake movement
 def move_snake(position, direction):
     x, y = position
     if direction == "up":
@@ -58,14 +66,17 @@ def move_snake(position, direction):
     else:
         raise ValueError(f"Invalid direction: {direction}")
 
+# Shecking collision with any sort of objects
 def check_collision(position1, position2):
     return position1 == position2
 
+# Checking if snake is withing the board
 def is_within_bounds(position, board_size):
     x, y = position
     width, height = board_size
     return 0 <= x < width and 0 <= y < height
 
+# Increasing speed with score
 def increase_speed(score):
     starting_speed = 1
     max_speed = 2

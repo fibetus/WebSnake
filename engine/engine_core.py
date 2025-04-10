6808497058595060e85813de3819e0954ad578fc
fibetus <s30331@pjwstk.edu.pl>
2025-04-11 00:23:18 +0200

    Add setup scene

--- engine/engine_core.py
+++ engine/engine_core.py
@@ -1,12 +1,26 @@
 class Game:
-    def __init__(self, board_size=(10, 10), initial_position=(5, 5), direction="right"):
+    def __init__(self, board_size=(10, 10), initial_position=None, direction="right"):
         self.board_size = board_size
-        self.snake = [initial_position]
+        width, height = board_size
+
+        # Set default initial position at center if not provided
+        if initial_position is None:
+            initial_position = (width // 2, height // 2)
+
+        # Make sure initial position is within bounds
+        init_x = max(0, min(initial_position[0], width - 1))
+        init_y = max(0, min(initial_position[1], height - 1))
+        self.snake = [(init_x, init_y)]
+
         self.direction = direction
-        self.food = (7, 5)
         self.score = 0
         self.game_over = False
         self.speed = 1
+
+        # Initialize food to None
+        self.food = None
+        # Spawn food after initializing snake
+        self.spawn_food()
 
     def update(self):
         if self.game_over:
@@ -14,8 +28,8 @@ class Game:
 
         new_head = move_snake(self.snake[0], self.direction)
 
-        # Checking if snake isn't withing bounds
-        if not is_within_bounds(new_head, self.board_size) or new_head == self.snake:
+        # Checking if snake isn't within bounds
+        if not is_within_bounds(new_head, self.board_size):
             self.game_over = True
             return
 
@@ -28,22 +42,33 @@ class Game:
         # Add "tail" to snake (more like adding new head and old head becomes tail)
         self.snake.insert(0, new_head)
 
-        if check_collision(new_head, self.food):
+        # Check if we've eaten food
+        if self.food and check_collision(new_head, self.food):
             self.score += 1
             self.speed = increase_speed(self.score)
             self.spawn_food()
         else:
-            self.snake.pop() # Pop if we don't eat anything
+            self.snake.pop()  # Pop if we don't eat anything
 
     def spawn_food(self):
-        from random import randint
         width, height = self.board_size
-        while True:
-            new_food = (randint(0, width - 1), randint(0, height - 1))
-            if new_food not in self.snake:
-                self.food = new_food
-                break
 
+        # Create a set of all possible positions
+        all_positions = set((x, y) for x in range(width) for y in range(height))
+
+        # Create a set of occupied positions (snake segments)
+        snake_positions = set(self.snake)
+
+        # Get available positions by removing snake positions from all positions
+        available_positions = list(all_positions - snake_positions)
+
+        # If there are available positions, choose one randomly
+        if available_positions:
+            import random
+            self.food = random.choice(available_positions)
+        else:
+            # No available positions, game won
+            self.game_over = True
 
     # Checking if we don't change direction into snake body
     def change_direction(self, new_direction):
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import argparse
 from engine import move_snake, check_collision, is_within_bounds, increase_speed, Game
 from ui import UI
-
-
+from data import PlayerData
 
 
 def main():
     parser = argparse.ArgumentParser(description="Simple Snake Game in Python")
     parser.add_argument("--history", help="Displays the history of Snake Game", action="store_true")
-
 
     args = parser.parse_args()
     if args.history:
@@ -17,11 +15,18 @@ def main():
               "The first home computer version 'Worm' was programmed by Peter Trefonas for the TRS-80 and published in 1978.\n"
               "Fun Fact: The Snake on IBM PC was rendered in a text mode!")
 
-    game = Game(board_size=(10, 10), initial_position=(5, 5), direction="right")
+    # Create player data storage
+    player_data = PlayerData()
 
-    ui = UI(game)
+    # Create UI without a game instance yet (we'll create it after setup)
+    ui = UI()
+
+    # Show setup scene first
+    ui.show_setup_scene(player_data)
+
+    # Start the main game (the game instance is created during setup)
     ui.start()
+
 
 if __name__ == '__main__':
     main()
-
